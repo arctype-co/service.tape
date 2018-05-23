d13d5a72bfcc44db0283b1b2c471c1516f6b3db4
Ryan Sundberg <ryan.sundberg@gmail.com>
2018-05-22 20:22:14 -0700

    Writable tape

--- src/arctype/service/tape.clj
+++ src/arctype/service/tape.clj
@@ -1,5 +1,5 @@
-(ns ^{:doc "Message I/O using Kafka"}
-  arctype.service.kafka
+(ns ^{:doc "File queue using Tape (by Square)"}
+  arctype.service.tape
   (:refer-clojure :exclude [key])
   (:import
     [com.squareup.tape2 QueueFile QueueFile$Builder])
@@ -13,25 +13,33 @@
     [arctype.service.protocol :refer :all]))
 
 (def Config
-  {:queues-path S/Str ; directory name for queue files
+  {:path S/Str ; directory name for queue files
    (S/optional-key :write-buffer-size) S/Int ; Size of write buffer (in objects, not bytes)
-   :topics [S/Str]})
+   })
+
+(def default-config
+  {:write-buffer-size 32})
 
 (defn put!
-  [this topic data]
-  
-  )
+  [{:keys [queues encoder]} topic data]
+  (if-let [queue-file (get queues topic)]
+    (let [buf (encoder data)]
+      (locking queue-file
+        (.add queue-file buf)))
+    (throw (ex-info "Can not put to undefined topic"
+                    {:topic topic
+                     :topics (:keys queues)}))))
 
 (defn- open-queue
   [this queue-name]
-  (let [file (io/file (str (:queues-path (:config this)) "/" queue-name ".tape"))]
+  (let [file (io/file (str (:path (:config this)) "/" queue-name ".tape"))]
     (.build (QueueFile$Builder. file))))
 
 (defn- open-queues
   [this]
-  (->> (:queue-names (:config this))
-       (map (fn [queue-name]
-              [(keyword queue-name) (open-queue this queue-name)]))
+  (->> (:topics this)
+       (map (fn [topic]
+              [topic (open-queue this (name topic))]))
        (into {})))
 
 (defn- close-queues
@@ -39,11 +47,23 @@
   (doseq [[queue-id queue-file] queues]
     (.close queue-file)))
 
+(defn- mkdir!
+  [path]
+  (let [dir (io/file path)]
+    (if (.exists dir)
+      (when-not (.isDirectory dir)
+        (throw (ex-info "Tape path is not a directory"
+                        {:path path})))
+      (when-not (.mkdirs dir)
+        (throw (ex-info "Failed to make tape directory"
+                        {:path path}))))))
+
 (defrecord TapeQ [config]
   PLifecycle
 
   (start [this]
     (log/info "Starting Tape service")
+    (mkdir! (:path config))
     (-> this
         (assoc :queues (open-queues this))))
 
@@ -61,10 +81,12 @@
 (S/defn create
   [resource-name
    config :- Config
-   encoder :- S/Any] ; (fn [dict] -> bytes) Data encoder)
+   encoder :- S/Any ; (fn [dict] -> bytes) Data encoder)
+   topics :- [S/Any]] ; list of keywords or string
   (let [config (merge default-config config)]
     (resource/make-resource
       (map->TapeQ
         {:config config
+         :topics topics
          :encoder encoder})
       resource-name)))
